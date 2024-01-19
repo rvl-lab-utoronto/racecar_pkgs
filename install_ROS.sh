d532d8c5bffed4bec33b1b2dc7eb8172592cf698
Ruthrash Hari <ruthrash.hari@gmail.com>
2024-01-19 14:10:50 -0500

    added bash script to install ROS noetic

--- install_ROS.sh
+++ install_ROS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd
+sudo apt-get update && sudo apt-get -y upgrade
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
-sudo apt install ros-melodic-desktop-full
-sudo apt install python-rosdep
-sudo rosdep init
-rosdep update
-
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update 
+sudo apt install -y ros-noetic-desktop-full
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc 
