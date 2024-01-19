0027ca8f66419fdcd5de106177a77e4c50d2ef9c
Ruthrash Hari <ruthrash.hari@gmail.com>
2024-01-19 14:11:20 -0500

    added VScode installation that works in 20.04 ubuntu

--- install_VSCode.sh
+++ install_VSCode.sh
@@ -3,7 +3,6 @@
 cd
 mkdir git 
 cd git 
-git clone https://github.com/JetsonHacksNano/installVSCode.git
-cd installVSCode
-./installVSCode.sh
+wget https://update.code.visualstudio.com/1.50.0/linux-deb-arm64/stable -O stable.deb
+sudo dpkg -i stable.deb
 
