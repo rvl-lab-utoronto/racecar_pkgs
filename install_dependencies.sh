2e60187b87140deee7a88344091755ccaea0f7d0
Ruthrash Hari <ruthrash.hari@gmail.com>
2024-01-22 09:55:20 -0500

    added installation of pip and python dependancies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install -y ros-noetic-joy ros-noetic-robot-pose-ekf ros-noetic-joy-teleop ros-noetic-robot-localization ros-noetic-urg-node ros-noetic-librealsense2 ros-noetic-realsense2-camera ros-noetic-realsense2-description ros-noetic-ackermann-msgs ros-noetic-serial
+sudo apt-get install -y ros-noetic-joy \
+        ros-noetic-robot-pose-ekf \
+        ros-noetic-joy-teleop \
+        ros-noetic-robot-localization \
+        ros-noetic-urg-node \
+        ros-noetic-librealsense2 \
+        ros-noetic-realsense2-camera \
+        ros-noetic-realsense2-description\
+        ros-noetic-ackermann-msgs \
+        ros-noetic-serial
+sudo apt-get install python3-pip
+pip3 install pyserial
 
 
 
